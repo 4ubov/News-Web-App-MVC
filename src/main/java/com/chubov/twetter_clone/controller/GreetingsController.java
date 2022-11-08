5d27ba24e87ab4098a8d2f997df2f345fdd527f2
= <chubov2002@gmail.com>
2022-11-08 21:36:58 +0600

    Добавлена возможность Писать сообещения. Фильтровать их по тегу. Подключено всё к MySQL.

--- src/main/java/com/chubov/twetter_clone/controller/GreetingsController.java
+++ src/main/java/com/chubov/twetter_clone/controller/GreetingsController.java
@@ -1,15 +1,22 @@
 package com.chubov.twetter_clone.controller;
 
+import com.chubov.twetter_clone.domain.Message;
+import com.chubov.twetter_clone.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingsController {
 
+    @Autowired
+    private MessageRepo messageRepo;
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model)
     {
@@ -18,8 +25,33 @@ public class GreetingsController {
     }
     @GetMapping
     public String main(Map<String, Object> model){
-        model.put("some", "It's a Main page, Hello!!");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+        //Сохраняем полученное сообщение из формы
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        //Выводим полученное сообщение
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
         return "main";
     }
 
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+        //Если значение фильтра пустое или null, то вывести все сообщения
+        if(filter==null || filter.isEmpty()){
+            messages = messageRepo.findAll();
+        }
+        //Сортировка по фильтру (по тегу)
+        else {
+            messages = messageRepo.findByTag(filter);
+        }
+        model.put("messages", messages);
+        return "main";
+    }
 }
---
+++ src/main/java/com/chubov/twetter_clone/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.chubov.twetter_clone.repos;
+
+import com.chubov.twetter_clone.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
