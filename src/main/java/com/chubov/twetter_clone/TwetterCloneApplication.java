7080ab8b8e415392a2030fcaf551adb6e891359f
= <chubov2002@gmail.com>
2022-11-08 14:08:21 +0600

    MVC Spring Boot Mustache project

---
+++ src/main/java/com/chubov/twetter_clone/TwetterCloneApplication.java
@@ -0,0 +1,13 @@
+package com.chubov.twetter_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwetterCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwetterCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chubov/twetter_clone/TwetterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chubov.twetter_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwetterCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
