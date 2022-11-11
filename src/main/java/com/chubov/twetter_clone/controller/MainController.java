4afad0e24ee2f5e45d7cf87a029c0091eaa8bfff
= <chubov2002@gmail.com>
2022-11-12 01:49:20 +0600

    Добавлена возможность загружать файлы на сервер. И раздавать их на страницу. Раздача статики.

--- src/main/java/com/chubov/twetter_clone/controller/MainController.java
+++ src/main/java/com/chubov/twetter_clone/controller/MainController.java
@@ -4,17 +4,24 @@ import com.chubov.twetter_clone.domain.Message;
 import com.chubov.twetter_clone.domain.User;
 import com.chubov.twetter_clone.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
+    @Value("${upload.path}")
+    private String uploadPath;
     @Autowired
     private MessageRepo messageRepo;
 
@@ -41,12 +48,24 @@ public class MainController {
 
     @PostMapping("/main")
     public String add(
+            @RequestParam("file")MultipartFile file,
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model){
+            Map<String, Object> model) throws IOException {
         //Сохраняем полученное сообщение из формы
         Message message = new Message(text, tag, user);
+        /*Загружаем файл по путю указаному в properties, если указанной дериктории нет, то ее создают*/
+        if (file!=null && !file.getOriginalFilename().isEmpty()){
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath+"/"+resultFileName));
+            message.setFilename(resultFileName);
+        }
 
         messageRepo.save(message);
         //Выводим полученное сообщение
