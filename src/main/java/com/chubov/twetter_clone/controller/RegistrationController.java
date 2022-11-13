46bdd0db765739acbca204a4c6f014de53f60978
= <chubov2002@gmail.com>
2022-11-13 16:28:09 +0600

    Добавлено поле Mail для User. Добавлена возможность активации аккаунта по почте. Рассылка на почту.

--- src/main/java/com/chubov/twetter_clone/controller/RegistrationController.java
+++ src/main/java/com/chubov/twetter_clone/controller/RegistrationController.java
@@ -1,21 +1,20 @@
 package com.chubov.twetter_clone.controller;
 
-import com.chubov.twetter_clone.domain.Role;
 import com.chubov.twetter_clone.domain.User;
-import com.chubov.twetter_clone.repos.UserRepo;
+import com.chubov.twetter_clone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collections;
 import java.util.Map;
-import java.util.Set;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -24,17 +23,27 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model) {
-        User userFromDb = userRepo.findByUsername(user.getUsername());
 
-        if (userFromDb != null) {
+        /*Если пользователь найден в БД, то вывести User exist,
+            иначе добавляем пользователя и редиректим на login page*/
+        if (!userService.addUser(user)) {
             model.put("message", "User exists!");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
-
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated){
+            model.addAttribute("message", "User successfully activated");
+        }else {
+            model.addAttribute("message", "Activation code is not found.");
+        }
+
+        return "login";
     }
 }
