6073de0cd9d1cd0ff35f516f43bbdf73b03de0db
= <chubov2002@gmail.com>
2022-11-13 19:46:54 +0600

    Добавлены миграции БД через Spring boot FlyWay. Добавлена страница Profile, возможность сменить почту и пароль с подтверждением по почте.

--- src/main/java/com/chubov/twetter_clone/domain/Message.java
+++ src/main/java/com/chubov/twetter_clone/domain/Message.java
@@ -8,7 +8,7 @@ import com.chubov.twetter_clone.domain.User;
 public class Message {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String text;
     private String tag;
@@ -46,11 +46,11 @@ public class Message {
         this.author = author;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/com/chubov/twetter_clone/service/UserService.java
+++ src/main/java/com/chubov/twetter_clone/service/UserService.java
@@ -11,8 +11,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -27,10 +27,10 @@ public class UserService implements UserDetailsService {
         return userRepo.findByUsername(username);
     }
 
-    public boolean addUser(User user){
+    public boolean addUser(User user) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
 
-        if(userFromDb != null){
+        if (userFromDb != null) {
             return false;
         }
 
@@ -39,23 +39,27 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
 
-        if (!StringUtils.isEmpty(user.getEmail())){
+        sendMessage(user);
+
+        return true;
+    }
+
+    private void sendMessage(User user) {
+        if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
                     "Hello, %s! \n" +
                             "Welcome to my web project - Twitter clone. \n" +
                             "Please, visit next link: http://localhost:8080/activate/%s",
-                            user.getUsername(), user.getActivationCode()
+                    user.getUsername(), user.getActivationCode()
             );
             mailSender.send(user.getEmail(), "Activation code", message);
         }
-
-        return true;
     }
 
     public boolean activateUser(String code) {
         User user = userRepo.findByActivationCode(code);
 
-        if (user == null ){
+        if (user == null) {
             return false;
         }
 
@@ -63,5 +67,43 @@ public class UserService implements UserDetailsService {
         userRepo.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String newPassword, String newEmail) {
+        String userEmail = user.getEmail();
+        boolean isEmailChanged = (newEmail != null && !newEmail.equals(userEmail) ||
+                (userEmail != null && !userEmail.equals(newEmail)));
+        if (isEmailChanged) {
+            user.setEmail(newEmail);
+            if (!StringUtils.isEmpty(newEmail)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+        if (StringUtils.isEmpty(newPassword)) {
+            user.setPassword(newPassword);
+        }
+
+        userRepo.save(user);
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
     }
 }
